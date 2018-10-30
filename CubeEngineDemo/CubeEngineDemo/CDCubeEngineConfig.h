2847b0df0edd5df4f8fbc3bd9c7e7ca6210dcda1
Ashine <ashine@AshinedeiMac.local>
2018-10-30 11:40:14 +0800

    fix config

--- CubeEngineDemo/CubeEngineDemo/CDCubeEngineConfig.h
+++ CubeEngineDemo/CubeEngineDemo/CDCubeEngineConfig.h
@@ -9,29 +9,12 @@
 #ifndef CDCubeEngineConfig_h
 #define CDCubeEngineConfig_h
 
-//////////////////////////////////// 测试服务器配置 //////////////////////////////////////////
-//#define APPID @"09eab471bc1d4a1b855f6b8f87b01caf800"
-//#define APPKey @"cc9793d0dd534cfc8c8f74e2f2ab1baf800"
-//#define GetTokenUrl @"http://125.208.1.67:4000/user/login"
-//#define GetLicenseUrl @"http://125.208.1.67:9000/auth/license/get"
-
-// 测试服务器配置
-#define APPID @"2556ce37c3434e1a9c67b1899125751a798"
-#define APPKey @"7f60c5f87e2a4dfab4c591e1145e9d5a799"
-#define GetTokenUrl @"http://125.208.1.67:4000/user/login"
-#define GetLicenseUrl @"http://dev.license.shixincube.cn/auth/license/get"
-
-// testflight配置
-//#define APPID @"6365f0cafd8a47b984bdc08a64327881"
-//#define APPKey @"9074ad1395f24fbd83a92ddc80facb1f"
-//#define GetTokenUrl @"http://125.208.1.67:4000/user/login"
-//#define GetLicenseUrl @"http://dev.license.shixincube.cn/auth/license/get"
 
 //////////////////////////////////// 正式服务器配置 //////////////////////////////////////////
-//#define APPID @"4cd8cf893f6342b08c0d6478b2de18f2"
-//#define APPKey @"b1781cf8deae4491ab7e212384c99774"
-//#define GetTokenUrl @"https://user.shixincube.com/cube/login"
-//#define GetLicenseUrl @"http://license.getcube.cn/auth/license/get"
+#define APPID @""  // Get appId & appKey from  https://www.shixincube.com
+#define APPKey @""
+#define GetTokenUrl @"https://user.shixincube.com/cube/login"
+#define GetLicenseUrl @"http://license.getcube.cn/auth/license/get"
 
 
 #endif /* CDCubeEngineConfig_h */
