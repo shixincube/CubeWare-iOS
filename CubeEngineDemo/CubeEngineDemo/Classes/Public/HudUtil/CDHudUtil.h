d94d10856980c45e6088e1312223acccc4bf3514
zhangdi <zhangdi@shixinyun.com>
2018-11-21 14:02:48 +0800

    增加注释文字

--- CubeEngineDemo/CubeEngineDemo/Classes/Public/HudUtil/CDHudUtil.h
+++ CubeEngineDemo/CubeEngineDemo/Classes/Public/HudUtil/CDHudUtil.h
@@ -10,9 +10,24 @@
 
 @interface CDHudUtil : NSObject
 
+/**
+ 显示toast
+
+ @param message 文字
+ @param duration 显示时长
+ @param block 回调
+ */
 + (void)showToastWithMessage:(NSString *)message
                     duration:(NSTimeInterval )duration
                   completion:(void(^)(void))block;
+
+/**
+ 显示
+ */
 + (void)showDefultHud;
+
+/**
+ 隐藏
+ */
 + (void)hideDefultHud;
 @end
